52cc5d739daccdbda7473f8174e473c40abb1808
wesley <wesochienga@gmail.com>
2025-09-23 15:34:01 +0300

    updated the age validation

--- main.py
+++ main.py
@@ -14,22 +14,35 @@ class User:
     #now for some validations
     #string
     @property
-    def details(self):
+    def name(self):
         return[self.__name,self.__age, self.__gender,self.__national_ID,self.__photo]
     
-    @details.getter
-    def details(self):
+    @name.getter
+    def name(self):
         print (f'This account belongs to a {self.__gender} user, {self.__name} of age {self.__age} of national ID {self.__national_ID}.')
 
-    @details.setter
-    def details(self,name_value): #  , age_value, gender_value, nationaId_value
+    @name.setter
+    def name(self,name_value): #  , age_value, gender_value, nationaId_value
         #name validation( check for the length, type of characters used)
         if isinstance(name_value, str) and name_value.isalpha() and 5 <= len(name_value) <= 20:
             self.__name = name_value
         else:
             raise ValueError (" invalid characters used")
+    @property
+    def age(self):
+        return (self.__age)
     
+    @age.setter
+    def age(self, age_value):
+        if isinstance(age_value,int) and not isinstance(age_value, bool) and 0 <= age_value <= 101:
+            self.__age = age_value
+        else:
+            raise ValueError (" invalid age")
+
+    #now for some age validation
 p1 = User("Bill", 21, "male", 112211, "jpg")
-p1.details
-p1.details = 'Conns'
-p1.details
+
+p1.name = 'constantine'
+p1.name
+p1.age =40
+p1.name
