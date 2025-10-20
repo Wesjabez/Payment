4d7f43d52623ef64efdb28bbcc5e85cb18ec3169
wesley <wesochienga@gmail.com>
2025-10-20 14:45:51 +0300

    image handling , type validation without a server

--- imageValidation.py
+++ imageValidation.py
@@ -9,6 +9,32 @@ def is_file_valid(filename):
     return os.path.splitext(filename)[1].lower() in [".jpg" , ".jpeg"]
 
 
-out = is_file_valid('')
+#####
+from PIL import Image
 
-print(out)
+def is_valid(filename):
+    try:
+        with Image.open(filename) as pasp:
+            return pasp.format.lower() in ["jpeg", "jpg"]
+    except:
+        return False
+    
+
+filename1 = 'templates/passport1.webp'
+filename2  ='templates/passport3.jpg'
+first = is_valid(filename1)
+second = is_valid(filename2)  
+
+
+print(first)
+print(second)
+
+def fileformat(filename):
+    fil = Image.open(filename).format
+    print(fil)
+    return fil
+fileformat(filename1)
+fileformat(filename2)
+
+
+
